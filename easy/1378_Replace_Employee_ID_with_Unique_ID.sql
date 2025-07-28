1deb5ca61e88fb87b02217830e5d2af9165e1302
sakshi-2603 <rajepandharesakshi@gmail.com>
2025-07-28 14:15:39 +0530

    Replace Employee ID with The Unique Identifier

---
+++ easy/1378_Replace_Employee_ID_with_Unique_ID.sql
@@ -0,0 +1,7 @@
+-- LeetCode 1378. Replace Employee ID with The Unique Identifier
+-- https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/
+
+SELECT euni.unique_id, e.name
+FROM Employees e
+LEFT JOIN EmployeeUNI euni
+  ON e.id = euni.id;
