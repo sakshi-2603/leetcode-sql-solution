84b2fa63241769cb6500a6d1a2fb9c76457c0618
sakshi-2603 <rajepandharesakshi@gmail.com>
2025-07-28 14:11:42 +0530

    Added solution for LeetCode 1068 - Product Sales Analysis

---
+++ easy/1068_Sales_Analysis_Report.sql
@@ -0,0 +1,7 @@
+-- LeetCode 1068. Product Sales Analysis I
+-- https://leetcode.com/problems/product-sales-analysis-i/
+
+SELECT p.product_name, s.year, s.price
+FROM Sales s
+JOIN Product p
+  ON s.product_id = p.product_id;
