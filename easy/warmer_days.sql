292627869d5d1c68fa35dc715f6d113e44be44b3
sakshi-2603 <rajepandharesakshi@gmail.com>
2025-07-28 14:44:40 +0530

    find all dates where the temperature was higher than the previous day

---
+++ easy/warmer_days.sql
@@ -0,0 +1,7 @@
+-- SQL to find all dates where the temperature was higher than the previous day
+
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2
+  ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;
