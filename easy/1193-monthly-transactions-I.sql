3ff50640d803a76adaa41bc0a2d4c9049dc9b325
sakshi-2603 <rajepandharesakshi@gmail.com>
2025-08-03 19:02:38 +0530

    Create 1193-monthly-transactions-I.sql

---
+++ easy/1193-monthly-transactions-I.sql
@@ -0,0 +1,12 @@
+SELECT 
+    ROUND(
+        SUM(order_date = customer_pref_delivery_date) * 100.0 / COUNT(*),
+        2
+    ) AS immediate_percentage
+FROM Delivery
+WHERE (customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date) AS first_order_date
+    FROM Delivery
+    GROUP BY customer_id
+);
+
