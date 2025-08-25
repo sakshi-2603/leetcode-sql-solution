d463707594eb55a0bc847787f2e93edeb7bb3bd7
sakshi-2603 <rajepandharesakshi@gmail.com>
2025-08-25 22:06:29 +0530

    Update second_highest_salary.sql

--- easy/second_highest_salary.sql
+++ easy/second_highest_salary.sql
@@ -1,6 +1,10 @@
-SELECT (
+1.SELECT (
     SELECT DISTINCT salary
     FROM Employee
     ORDER BY salary DESC
     LIMIT 1 OFFSET 1
 ) AS SecondHighestSalary;
+
+2.SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary < (SELECT MAX(salary) FROM Employee);
